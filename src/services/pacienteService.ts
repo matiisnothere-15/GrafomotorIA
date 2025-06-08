31418cbbd22c555e0cb2fe6d8bd42e54f5b070db
Pablo <pab.aravena.16@gmail.com>
2025-06-07 20:50:42 -0400

    [feature] connect table seguimiento, sesion and paciente

---
+++ src/services/pacienteService.ts
@@ -0,0 +1,50 @@
+import { BASE_URL } from "../config.ts";
+import type { Paciente } from "../models/Paciente.ts";
+
+export const getPacientes = async (): Promise<Paciente[]> => {
+  const res = await fetch(`${BASE_URL}/paciente`, {
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (!res.ok) throw new Error("Error al obtener pacientes");
+  return await res.json();
+};
+
+export const getPacienteById = async (id: number): Promise<Paciente> => {
+  const res = await fetch(`${BASE_URL}/paciente/${id}`, {
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (!res.ok) throw new Error("Paciente no encontrado");
+  return await res.json();
+};
+
+export const createPaciente = async (data: Paciente) => {
+  const res = await fetch(`${BASE_URL}/paciente`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) throw new Error("Error al crear paciente");
+  return await res.json();
+};
+
+export const updatePaciente = async (id: number, data: Paciente) => {
+  const res = await fetch(`${BASE_URL}/paciente/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  });
+
+  if (!res.ok) throw new Error("Error al actualizar paciente");
+  return await res.json();
+};
+
+export const deletePaciente = async (id: number) => {
+  const res = await fetch(`${BASE_URL}/paciente/${id}`, {
+    method: "DELETE",
+  });
+
+  if (!res.ok) throw new Error("Error al eliminar paciente");
+};
